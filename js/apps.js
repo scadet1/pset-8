0117015989cf0f47832af344894fcb52d8b870b6
Ritshy <scadet1@ucvts.org>
2020-02-09 17:48:43 -0500

    I hate my life

--- js/apps.js
+++ js/apps.js
@@ -1,109 +1,80 @@
-
 ///////////////////// CONSTANTS /////////////////////////////////////
 const winningConditions = [
-[0, 1, 2],
-[3, 4, 5],
-[6, 7, 8],
-[0, 3, 6],
-[1, 4, 7],
-[2, 5, 8],
-[0, 4, 8],
-[2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ];
-
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
+
 let board;
 let turn;
 let win;
-let scoreX = 0;
-let scoreO = 0;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+
 const squares = Array.from(document.querySelectorAll("#board div"));
-const message = document.querySelector("h2");
-const scoreXMessage = document.querySelector("h3");
-const scoreOMessage = document.querySelector("h4");
+const message = document.querySelector("h2");   // grab the subheader
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
+
 window.onload = init;
 document.getElementById("board").onclick = takeTurn;
 document.getElementById("reset-button").onclick = init;
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
+
 function init() {
-board = [
-  "", "", "",
-  "", "", "",
-  "", "", ""
-];
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
 
-do {
-  var initialPlayer = prompt("Enter X or O to declare the first player: ");
-  if (initialPlayer === null) {
-    turn = "X";
-    break;
-  } else if (initialPlayer === "X" || initialPlayer === "x") {
-    turn = "X";
-  } else if (initialPlayer === "O" || initialPlayer === "o") {
-    turn = "O";
-  } else {
-    //intentially blank
-  }
-} while (initialPlayer !== "X" && initialPlayer !== "x" && initialPlayer !== "O" && initialPlayer !== "o");
+  turn = "X";
+  win = null;
 
-win = null;
-
-render();
+  render();   // we'll write this later
 }
-
 function render() {
-board.forEach(function(mark, index) {
-  squares[index].textContent = mark;
-});
-
-message.textContent =
-  win === "T" ? "It's a tie!"
-  : win ? `${win} wins!`
-  : `Turn: ${turn}`;
-if (win === "X") {
-  scoreX++;
-  scoreXMessage.textContent = scoreX;
-} else if (win === "O") {
-  scoreO++;
-  scoreOMessage.textContent = scoreO;
-} else {
-  //if there is a tie, do not change score
-}
-}
-
-function takeTurn(e) {
-if (!win) {
-  let index = squares.findIndex(function(square) {
-    return square === e.target;
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;
   });
 
-  if (board[index] === "") {
-    board[index] = turn;
-    turn = turn === "X" ? "O" : "X";
-    win = getWinner();
+  message.textContent =
+    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
+}
+function takeTurn(e) {
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
 
-    render();
+    if (board[index] === "") {
+      board[index] = turn;
+      turn = turn === "X" ? "O" : "X";
+      win = getWinner();
+
+      render();
     }
   }
 }
-
 function getWinner() {
-let winner = null;
+  let winner = null;
 
-winningConditions.forEach(function(condition, index) {
-  if (
-    board[condition[0]] &&
-    board[condition[0]] === board[condition[1]] &&
-    board[condition[0]] === board[condition[2]]
-  ){
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
       winner = board[condition[0]];
-  }
-});
+    }
+  });
 
-return winner ? winner : board.includes("") ? null : "T";
+  return winner ? winner : board.includes("") ? null : "T";
 }
